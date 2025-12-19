6f1b35d2845a15806b7dea7f8ab641ad334192cb
teeradui <teerada_b@cmu.ac.th>
2025-12-19 10:11:55 +0700

    array listtile

--- lib/pages/contact.dart
+++ lib/pages/contact.dart
@@ -1,3 +1,4 @@
+import 'package:fistapp/pages/calc.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -9,26 +10,23 @@ class ContactPage extends StatefulWidget {
 }
 
 class _ContactPageState extends State<ContactPage> {
+  final items = List<String>.generate(100, (i) => "Item $i");
   @override
   Widget build(BuildContext context) {
     List mydata = ['apple', 'banana', 'papaya'];
-    return ListView(
-      children: [
-        ListTile(onTap: () {}, 
-        leading: Icon(Icons.apple), 
-        title: Text(mydata[0]),
-        subtitle: Text('This is an apple'),
-        trailing: FlutterLogo(),
-        ),
-        ListTile(onTap: () {}, 
-        leading: FlutterLogo(), 
-        title: Text(mydata[1])
-        ),
-        ListTile(onTap: () {}, 
-        leading: FlutterLogo(), 
-        title: Text(mydata[2])
-        ),
-      ],
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return ListTile(
+          onTap: () {
+            Navigator.push(context,MaterialPageRoute(
+              builder: (context)=> const CalculatePage())
+              );
+          },
+          leading: Icon(Icons.apple),
+          title: Text(mydata[index]),
+        );
+      },
+      itemCount: mydata.length,
     );
   }
 }
