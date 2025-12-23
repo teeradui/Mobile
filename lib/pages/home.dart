a9d5c29ae1af5db3ac342d85a6dfc8859027e5a6
teeradui <teerada_b@cmu.ac.th>
2025-12-23 10:40:38 +0700

    link MyBox with Detail page and done 7.1

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,3 +1,4 @@
+import 'package:fistapp/pages/detail.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -14,17 +15,29 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Let's know about Duidui!"),
-        backgroundColor: const Color.fromARGB(255, 255, 156, 189)
+        backgroundColor: const Color.fromARGB(255, 255, 156, 189),
       ),
       body: Padding(
         padding: const EdgeInsets.all(24.0),
         child: ListView(
           children: [
-              MyBox("What is Duidui", "Duidui is my work for this class!","https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExcHg4a3JzZXVsZDFyeDFpbjhqODcwc2QwZWltdGVudHc1bXB6amQ0MyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/VKwspRV2pafJu/giphy.gif"),
-              SizedBox(height: 10),
-              MyBox("What is Duidui mean", "Duidui doesn't have meaning, it's just a name(❁´◡`❁)","https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExczJpaG5mMGxuM25uZm1hYnM0ZXczYW5ub2d6cTVpYWtpbjJ1bTczNSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/GvTSAdT3MjBtWtDpJ0/giphy.gif"),
-              SizedBox(height: 10),
-              MyBox("Why Duidui", "Because I like the sound of this name! (｡•̀ᴗ-)✧","https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExcmN2ZGIzdnJlOGN5N28yOHFzd3F4OGVqMHhjdmdiZjZmcGR6cDc3aiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/hHZfOv3tpyItG/giphy.gif"),
+            MyBox(
+              "What is Duidui",
+              "Duidui is my work for this class!",
+              "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExcHg4a3JzZXVsZDFyeDFpbjhqODcwc2QwZWltdGVudHc1bXB6amQ0MyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/VKwspRV2pafJu/giphy.gif",
+            ),
+            SizedBox(height: 10),
+            MyBox(
+              "What is Duidui mean",
+              "Duidui doesn't have meaning, it's just a name(❁´◡`❁)",
+              "https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExczJpaG5mMGxuM25uZm1hYnM0ZXczYW5ub2d6cTVpYWtpbjJ1bTczNSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/GvTSAdT3MjBtWtDpJ0/giphy.gif",
+            ),
+            SizedBox(height: 10),
+            MyBox(
+              "Why Duidui",
+              "Because I like the sound of this name! (｡•̀ᴗ-)✧",
+              "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExcmN2ZGIzdnJlOGN5N28yOHFzd3F4OGVqMHhjdmdiZjZmcGR6cDc3aiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/hHZfOv3tpyItG/giphy.gif",
+            ),
           ],
         ),
       ),
@@ -38,9 +51,13 @@ class _HomePageState extends State<HomePage> {
       decoration: BoxDecoration(
         color: const Color.fromARGB(255, 255, 201, 219),
         borderRadius: BorderRadius.circular(20),
-        image: DecorationImage(image: NetworkImage(img_url)
-        , fit: BoxFit.cover,
-        colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.4), BlendMode.darken)
+        image: DecorationImage(
+          image: NetworkImage(img_url),
+          fit: BoxFit.cover,
+          colorFilter: ColorFilter.mode(
+            Colors.black.withOpacity(0.4),
+            BlendMode.darken,
+          ),
         ),
       ),
       child: Column(
@@ -48,18 +65,28 @@ class _HomePageState extends State<HomePage> {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
-            title, 
+            title,
             style: TextStyle(
               fontSize: 25,
-               color: Colors.white, 
-               fontWeight: FontWeight.bold),
-               ),
-          Text(
-            subtitle, 
-            style: TextStyle(
-              fontSize: 16, 
-              color: Colors.white),
-              ),
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(subtitle, style: TextStyle(fontSize: 16, color: Colors.white)),
+          SizedBox(height: 10),
+          TextButton(
+            onPressed: () {
+              print("Next Page");
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => DetailPage()),
+              );
+            },
+            child: Text(
+              "read more",
+              style: TextStyle(color: const Color.fromARGB(255, 255, 151, 193)),
+            ),
+          ),
         ],
       ),
     );
